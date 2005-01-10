be5971bb24aaa8c13ef1368d88c3bc7b64bd7a35
toady <toady@4fd5081c-5feb-0310-ae58-96eeab14693c>
2005-01-10 16:32:08 +0000

    autotoolized

---
+++ trunk/autoclean.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+rm -f aclocal.m4
+rm -f configure
+rm -rf autom4te.cache
+rm -f depcomp
+rm -f install-sh
+rm -f missing
+rm -f stamp-h1
+rm -f pvs.conf
+rm -f config.h
+rm -f config.log
+rm -f config.sub
+rm -f config.guess
+rm -f config.status
+rm -f INSTALL
+rm -f ltmain.sh
+rm -f *~
+rm -f Makefile.in
+rm -f Makefile
+rm -f libtool
